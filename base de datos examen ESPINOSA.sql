898393c70656bdcf36086f6feb6e31272e16755e
francisco-2212 <80895215+francisco-2212@users.noreply.github.com>
2021-06-25 14:56:33 -0300

    Add files via upload

---
+++ base de datos examen ESPINOSA.sql
@@ -0,0 +1,8 @@
+create database examen;
+use examen;
+create table estudiantes(
+idestudiante int not null primary key,
+nombre varchar (12) not null,
+apellido varchar (12) not null,
+nota int not null,
+curso varchar (40) not null);
