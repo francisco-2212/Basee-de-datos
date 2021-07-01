ce3d9916bc55245904049238e42fc23350ecd321
francisco-2212 <80895215+francisco-2212@users.noreply.github.com>
2021-07-01 09:46:09 -0300

    Delete base de datos examen ESPINOSA.sql

--- base de datos examen ESPINOSA.sql
+++
@@ -1,8 +0,0 @@
-create database examen;
-use examen;
-create table estudiantes(
-idestudiante int not null primary key,
-nombre varchar (12) not null,
-apellido varchar (12) not null,
-nota int not null,
-curso varchar (40) not null);
